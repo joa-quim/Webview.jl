594f9678414cde379ff360964d5063b86949fb26
Joaquim Luis <jmfluis@gmail.com>
2018-11-05 21:01:42 +0000

    Needed to qualify fun name

--- src/doc.jl
+++ src/doc.jl
@@ -2,7 +2,7 @@ function doc(fname::AbstractString; title::AbstractString="", width::Integer=0, 
 
 	if (width == 0)  width = 800  end
 	if (height == 0) height = 600 end
-	if (title == "") title = basename(fname)  end
-	MD = parse_file(fname);
+	if (title == "") title, = splitext(basename(fname))  end
+	MD = Markdown.parse_file(fname);
 	webview(title, "data:text/html,<html>" * html(MD) * "</html>", width, height)
 end
