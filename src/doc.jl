868a23e918f2cbdf40cd3793c3f43da390ccc20b
Joaquim Luis <jmfluis@gmail.com>
2018-11-06 15:41:55 +0000

    Deal also with URLs

--- src/doc.jl
+++ src/doc.jl
@@ -1,8 +1,12 @@
 function doc(fname::AbstractString; title::AbstractString="", width::Integer=0, height::Integer=0)
 
-	if (width == 0)  width = 800  end
-	if (height == 0) height = 600 end
+	if (width == 0)  width  = 800  end
+	if (height == 0) height = 600  end
 	if (title == "") title, = splitext(basename(fname))  end
-	MD = Markdown.parse_file(fname);
-	webview(title, "data:text/html,<html>" * html(MD) * "</html>", width, height)
+	if (startswith(fname, "http") || startswith(fname, "ftp") || startswith(fname, "file"))
+		webview(title, fname, width, height)
+	else
+		MD = Markdown.parse_file(fname);
+		webview(title, "data:text/html,<html>" * html(MD) * "</html>", width, height)
+	end
 end
